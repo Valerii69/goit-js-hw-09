78fa4c33d136a8492bc8dd919cfd6b74dc647592
Valerii Viun <2455521@gmail.com>
2023-04-13 21:03:41 +0300

    fix 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let Interval = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+btnStart.addEventListener('click', () => {
+  Interval = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+
+  btnStart.setAttribute('disabled', 'disabled');
+  btnStop.removeAttribute('disabled');
+});
+
+btnStop.addEventListener('click',() => {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'disabled');
+  clearInterval(Interval);
+});
